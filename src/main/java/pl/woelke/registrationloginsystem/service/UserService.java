b4ca1470e7214e029933e474dbe89627161f522e
Krzysztof <krzysiek@woelke.pl>
2023-03-02 01:27:39 +0100

    Add listView of registered users

--- src/main/java/pl/woelke/registrationloginsystem/service/UserService.java
+++ src/main/java/pl/woelke/registrationloginsystem/service/UserService.java
@@ -3,8 +3,12 @@ package pl.woelke.registrationloginsystem.service;
 import pl.woelke.registrationloginsystem.dto.UserDto;
 import pl.woelke.registrationloginsystem.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     void saveUser(UserDto userDto);
 
     User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
 }
